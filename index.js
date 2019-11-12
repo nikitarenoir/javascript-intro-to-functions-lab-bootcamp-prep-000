e716b3d79470b5524a6442dc44522cacc7e9c479
lively-intelligence-0477 <nickyrenwick99@hotmail.com>
2019-11-12 16:23:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function logWhisper (string) {
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma (string) {
-  return ("I can't hear you!") 
+  return ("I can't hear you!") ;
   if (string) === LowerCase 
 }
